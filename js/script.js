7eb1202268cf6bdb4a9241f0bfcdac3a64aa9ca2
Mohamed Elrfaay <56503517+mohamedeld@users.noreply.github.com>
2020-11-22 01:13:01 +0200

    about me

---
+++ js/script.js
@@ -0,0 +1,60 @@
+$(document).ready(function () {
+    $(window).on('load',function () {
+        $('.preloader').addClass('complete');
+    });
+    $(window).on('scroll',function () {
+        var scroll= $(window).scrollTop();
+        if(scroll >=50){
+            $(".sticky").addClass("stickyadd");
+        }else{
+            $(".sticky").removeClass("stickyadd");
+        }
+    })
+    var typed=new Typed(".element",{
+        strings:["Mohamed Elrfaay","a Developer","a Designer","a Web Developer"],
+        smartBackspace:true,
+        typeSpeed:100,
+        backSpeed:100,
+        loop:true,
+        loopCount:Infinity,
+        startDelay:1000
+    });
+    var waypoint = new Waypoint({
+        element: document.getElementById('exp-id'),
+        handler: function(direction) {
+            var p =document.querySelectorAll('.progress-bar');
+            p[0].setAttribute("style","width:100%;transition:1s all")
+            p[1].setAttribute("style","width:95%;transition:1.5s all")
+            p[2].setAttribute("style","width:90%;transition:1.7s all")
+            p[3].setAttribute("style","width:80%;transition:2s all")
+            p[4].setAttribute("style","width:75%;transition:2.3s all")
+            p[5].setAttribute("style","width:70%;transition:2.5s all")
+        },
+        offset:'90%'
+    });
+
+    /* filter js */
+    $(document).ready(function () {
+        $('.list').click(function () {
+            const value = $(this).attr('data-filter');
+            if (value == 'all') {
+                $('.itemBox').show('1000');
+            } else {
+                $('.itemBox').not('.' + value).hide('1000');
+                $('.itemBox').filter('.' + value).show('1000');
+            }
+        })
+
+        $('.list').click(function () {
+            $(this).addClass('active').siblings().removeClass('active');
+        })
+    });
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        autoplay:true,
+        autoplayTimeout: 4000,
+        items:1
+    });
+
+})
